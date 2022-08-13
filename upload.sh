b76ed76f3b51af21facde2fa84e9abead9f60bd3
PacoReinaCampo <frareicam@gmail.com>
2022-08-13 12:47:50 +0200

    Working in PU-NTM 🦀

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 git config --global user.email "frareicam@gmail.com"
 git config --global user.name "PacoReinaCampo"
 git add *
-git commit -m "Working in PU-NTM"
+git commit -m "Working in PU-NTM 🦀"
 git push origin master
