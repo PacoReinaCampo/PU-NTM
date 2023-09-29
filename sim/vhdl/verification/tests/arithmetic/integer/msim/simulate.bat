adc9185bef983adfdcce2e1a0e09c4fbd8562c54
PacoReinaCampo <frareicam@gmail.com>
2023-09-29 12:18:01 +0400

    Working in PU-NTM 🦀

--- sim/vhdl/verification/tests/arithmetic/integer/msim/simulate.bat
+++ sim/vhdl/verification/tests/arithmetic/integer/msim/simulate.bat
@@ -1,7 +1,18 @@
 @echo off
 call ../../../../../../../settings64_msim.bat
 
-vlib work
-vcom -2008 -f system.vc
-vsim -c -do run.do work.accelerator_integer_testbench
+vsim -c -do macros/run.do
+
+vsim -view wlf/accelerator_scalar_integer_adder_test.wlf -do waves/accelerator_scalar_integer_adder.do
+REM vsim -view wlf/accelerator_scalar_integer_multiplier.wlf -do waves/accelerator_scalar_integer_multiplier.do
+REM vsim -view wlf/accelerator_scalar_integer_divider.wlf -do waves/accelerator_scalar_integer_divider.do
+REM vsim -view wlf/accelerator_vector_integer_adder.wlf -do waves/accelerator_vector_integer_adder.do
+REM vsim -view wlf/accelerator_vector_integer_multiplier.wlf -do waves/accelerator_vector_integer_multiplier.do
+REM vsim -view wlf/accelerator_vector_integer_divider.wlf -do waves/accelerator_vector_integer_divider.do
+REM vsim -view wlf/accelerator_matrix_integer_adder.wlf -do waves/accelerator_matrix_integer_adder.do
+REM vsim -view wlf/accelerator_matrix_integer_multiplier.wlf -do waves/accelerator_matrix_integer_multiplier.do
+REM vsim -view wlf/accelerator_matrix_integer_divider.wlf -do waves/accelerator_matrix_integer_divider.do
+REM vsim -view wlf/accelerator_tensor_integer_adder.wlf -do waves/accelerator_tensor_integer_adder.do
+REM vsim -view wlf/accelerator_tensor_integer_multiplier.wlf -do waves/accelerator_tensor_integer_multiplier.do
+REM vsim -view wlf/accelerator_tensor_integer_divider.wlf -do waves/accelerator_tensor_integer_divider.do
 pause
--- sim/vhdl/verification/tests/arithmetic/integer/msim/simulate.sh
+++ sim/vhdl/verification/tests/arithmetic/integer/msim/simulate.sh
@@ -1,4 +1,17 @@
 source ../../../../../../../settings64_msim.sh
 
 make clean
-make
+make build
+
+make simulate TARGET=accelerator_scalar_integer_adder
+#make simulate TARGET=accelerator_scalar_integer_multiplier
+#make simulate TARGET=accelerator_scalar_integer_divider
+#make simulate TARGET=accelerator_vector_integer_adder
+#make simulate TARGET=accelerator_vector_integer_multiplier
+#make simulate TARGET=accelerator_vector_integer_divider
+#make simulate TARGET=accelerator_matrix_integer_adder
+#make simulate TARGET=accelerator_matrix_integer_multiplier
+#make simulate TARGET=accelerator_matrix_integer_divider
+#make simulate TARGET=accelerator_tensor_integer_adder
+#make simulate TARGET=accelerator_tensor_integer_multiplier
+#make simulate TARGET=accelerator_tensor_integer_divider
